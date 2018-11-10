703f8afc1e36076f92bf67ea0ae656c5806e0c03
Pavlo <ur5evw12@gmail.com>
2018-11-10 23:02:14 +0100

    Add BFS using to find SAP and distance + tests arrays

--- Part2/WordNet/src/WordNet.java
+++ Part2/WordNet/src/WordNet.java
@@ -1,4 +1,37 @@
+import edu.princeton.cs.algs4.BreadthFirstDirectedPaths;
+import edu.princeton.cs.algs4.Digraph;
+import edu.princeton.cs.algs4.In;
+
+import java.util.HashMap;
+
 public class WordNet {
+
+    private Digraph digraph;
+    private HashMap<String, Integer> values;
+    private HashMap<Integer, String> keys;
+    private int distance;
+    private int sap;
+
+    public WordNet() {
+        digraph = new Digraph(0);
+        values = new HashMap<>();
+        keys = new HashMap<>();
+    }
+
+    public WordNet(int[][] edges, int count) {
+        digraph = new Digraph(count);
+        values = new HashMap<>();
+        keys = new HashMap<>();
+
+        for (int[] edge: edges) {
+            digraph.addEdge(edge[0], edge[1]);
+        }
+
+        for (int i = 0; i < count; i++) {
+            keys.put(i, String.valueOf(i));
+            values.put(String.valueOf(i), i);
+        }
+    }
 
     // constructor takes the name of the two input files
     public WordNet(String synsets, String hypernyms) {
@@ -6,13 +39,40 @@ public class WordNet {
             throw new IllegalArgumentException();
         }
 
+        values = new HashMap<>();
+        In fileSynsets = new In(synsets);
+        In fileHypernyms = new In(hypernyms);
+
+        while (fileSynsets.hasNextLine()) {
+            String[] line = parseLine(fileSynsets);
+            int v = Integer.parseInt(line[0]);
+            String word = line[1];
+            values.put(word, v);
+            keys.put(v, word);
+        }
+
+        digraph = new Digraph(values.size());
+
+        while (fileHypernyms.hasNextLine()) {
+            String[] line = parseLine(fileHypernyms);
+            int v = Integer.parseInt(line[0]);
+            for (int i = 1; i < line.length; i++) {
+                int w = Integer.parseInt(line[i]);
+                digraph.addEdge(v, w);
+            }
+        }
+
         // should throw an exception if
         // the input to the constructor does not correspond to a rooted DAG
     }
 
+    private String[] parseLine(In in) {
+        return in.readLine().split(",");
+    }
+
     // returns all WordNet nouns
     public Iterable<String> nouns() {
-        return null;
+        return values.keySet();
     }
 
     // is the word a WordNet noun?
@@ -21,9 +81,7 @@ public class WordNet {
             throw new IllegalArgumentException();
         }
 
-        // should throw an exception if not WordNet noun
-
-        return true;
+        return values.containsValue(word);
     }
 
     // distance between nounA and nounB (defined below)
@@ -32,7 +90,28 @@ public class WordNet {
             throw new IllegalArgumentException();
         }
 
-        return 0;
+        runBfs(nounA, nounB);
+
+        return distance;
+    }
+
+    private void runBfs(String nounA, String nounB) {
+        int vertexA = values.get(nounA);
+        int vertexB = values.get(nounB);
+
+        Digraph reGraph = digraph.reverse();
+        distance = Integer.MAX_VALUE;
+        sap = 0;
+        for (int v = 0; v < digraph.V(); v++) {
+            BreadthFirstDirectedPaths bfs = new BreadthFirstDirectedPaths(reGraph, v);
+            if (bfs.hasPathTo(vertexA) && bfs.hasPathTo(vertexB)) {
+                var curr = bfs.distTo(vertexA) + bfs.distTo(vertexB);
+                if (distance > curr) {
+                    distance = curr;
+                    sap = v;
+                }
+            }
+        }
     }
 
     // a synset (second field of synsets.txt) that is the common ancestor of nounA and nounB
@@ -42,11 +121,13 @@ public class WordNet {
             throw new IllegalArgumentException();
         }
 
-        return null;
+        runBfs(nounA, nounB);
+
+        return keys.get(sap);
     }
 
     // do unit testing of this class
     public static void main(String[] args) {
-        // optional
+        WordNet wordNet = new WordNet(args[0], args[1]);
     }
 }
--- Part2/WordNet/src/WordNetShould.java
+++ Part2/WordNet/src/WordNetShould.java
@@ -1,10 +1,52 @@
+import org.junit.Assert;
 import org.junit.Test;
 
 public class WordNetShould {
     private WordNet wordNet;
 
+    private final int example1_verticesCount = 12;
+    private final int[][] example1 = {
+            new int[] { 1, 0 },
+            new int[] { 2, 0 },
+
+            new int[] { 3, 1 },
+            new int[] { 4, 1 },
+            new int[] { 5, 1 },
+
+            new int[] { 6, 3 },
+            new int[] { 7, 6 },
+            new int[] { 8, 5 },
+            new int[] { 9, 5 },
+
+            new int[] { 10, 9 },
+            new int[] { 11, 9 }
+    };
+
+    private final int example2_verticesCount = 6;
+    private final int[][] example2 = {
+            new int[] { 1, 0 },
+            new int[] { 1, 2 },
+            new int[] { 2, 3 },
+            new int[] { 3, 4 },
+            new int[] { 4, 5 },
+            new int[] { 5, 0 }
+    };
+
+    private final int example3_verticesCount = 10;
+    private final int[][] example3 = {
+            new int[] { 1, 0 },
+            new int[] { 2, 0 },
+            new int[] { 7, 1 },
+            new int[] { 3, 1 },
+            new int[] { 4, 2 },
+            new int[] { 5, 2 },
+            new int[] { 5, 6 },
+            new int[] { 8, 3 },
+            new int[] { 9, 3 }
+    };
+
     public WordNetShould() {
-        wordNet = new WordNet("synset", "hypermym");
+        wordNet = new WordNet();
     }
 
     @Test(expected = IllegalArgumentException.class)
@@ -20,5 +62,59 @@ public class WordNetShould {
     @Test(expected = IllegalArgumentException.class)
     public void ThrowAnIllegalArgumentExceptionInSapFunction() {
         wordNet.sap(null, null);
+    }
+
+    @Test
+    public void ReturnCorrectDistanceFromExample1() {
+        wordNet = new WordNet(example1, example1_verticesCount);
+        var nounA = "3";
+        var nounB = "10";
+        var distance = wordNet.distance(nounA, nounB);
+        Assert.assertEquals(4, distance);
+    }
+
+    @Test
+    public void ReturnCorrectSapFromExample1() {
+        wordNet = new WordNet(example1, example1_verticesCount);
+        var nounA = "3";
+        var nounB = "10";
+        var sap = wordNet.sap(nounA, nounB);
+        Assert.assertEquals("1", sap);
+    }
+
+    @Test
+    public void ReturnCorrectDistanceFromExample2() {
+        wordNet = new WordNet(example2, example2_verticesCount);
+        var nounA = "1";
+        var nounB = "5";
+        var distance = wordNet.distance(nounA, nounB);
+        Assert.assertEquals(2, distance);
+    }
+
+    @Test
+    public void ReturnCorrectSapFromExample2() {
+        wordNet = new WordNet(example2, example2_verticesCount);
+        var nounA = "1";
+        var nounB = "5";
+        var sap = wordNet.sap(nounA, nounB);
+        Assert.assertEquals("0", sap);
+    }
+
+    @Test
+    public void ReturnCorrectDistanceFromExample3() {
+        wordNet = new WordNet(example3, example3_verticesCount);
+        var nounA = "7";
+        var nounB = "9";
+        var distance = wordNet.distance(nounA, nounB);
+        Assert.assertEquals(3, distance);
+    }
+
+    @Test
+    public void ReturnCorrectSapFromExample3() {
+        wordNet = new WordNet(example3, example3_verticesCount);
+        var nounA = "7";
+        var nounB = "9";
+        var sap = wordNet.sap(nounA, nounB);
+        Assert.assertEquals("1", sap);
     }
 }
