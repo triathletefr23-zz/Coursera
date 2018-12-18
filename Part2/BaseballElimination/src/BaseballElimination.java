ce5d7c05fa38de7324b2816b4b905e91021163d0
Pavlo LIAHUSHYN <ur5evw12@gmail.com>
2018-12-18 23:33:29 +0100

    Add implementation for basic methods

--- Part2/BaseballElimination/src/BaseballElimination.java
+++ Part2/BaseballElimination/src/BaseballElimination.java
@@ -1,10 +1,23 @@
 import edu.princeton.cs.algs4.In;
-import edu.princeton.cs.algs4.Stack;
-
-import java.util.Arrays;
+import java.util.HashMap;
 
 public class BaseballElimination {
-    private final String[] teams;
+    private final class TeamResult {
+        private int wins;
+        private int losses;
+        private int left;
+        private int index;
+
+        private TeamResult(int wins, int losses, int left, int index) {
+            this.wins = wins;
+            this.losses = losses;
+            this.left = left;
+            this.index = index;
+        }
+    }
+
+    private final int[][] results;
+    private HashMap<String, TeamResult> teams;
 
     // create a baseball division from given filename in format specified below
     public BaseballElimination(String filename) {
@@ -13,54 +26,91 @@ public class BaseballElimination {
         }
 
         In in = new In(filename);
-
         int number = Integer.parseInt(in.readLine());
-        teams = new String[number];
+        teams = new HashMap<>();
+        results = new int[number][number];
 
-        int iter = 0;
+        int iterator = 0;
         while (in.hasNextLine()) {
-            teams[iter] = in.readLine();
+            var index = 0;
+            String[] infos = in.readLine().split("\\s+");
+            teams.put(infos[index++],
+                    new TeamResult(
+                    Integer.parseInt(infos[index++]),
+                    Integer.parseInt(infos[index++]),
+                    Integer.parseInt(infos[index++]),
+                            iterator));
 
+            for (int i = 0; i < number; i++) {
+                results[iterator][i] = Integer.parseInt(infos[index++]);
+            }
+            iterator++;
         }
     }
 
     // number of teams
     public int numberOfTeams() {
-        return teams.length;
+        return teams.size();
     }
 
     // all teams
     public Iterable<String> teams() {
-        return Arrays.asList(teams);
+        return teams.keySet();
     }
 
     // number of wins for given team
     public int wins(String team) {
+        if (team == null || !teams.containsKey(team)) {
+            throw new IllegalArgumentException();
+        }
 
+        return teams.get(team).wins;
     }
 
     // number of losses for given team
     public int losses(String team) {
+        if (team == null || !teams.containsKey(team)) {
+            throw new IllegalArgumentException();
+        }
 
+        return teams.get(team).losses;
     }
 
     // number of remaining games for given team
     public int remaining(String team) {
+        if (team == null || !teams.containsKey(team)) {
+            throw new IllegalArgumentException();
+        }
 
+        return teams.get(team).left;
     }
 
     // number of remaining games between team1 and team2
     public int against(String team1, String team2) {
+        if (team1 == null || team2 == null || !teams.containsKey(team1) || !teams.containsKey(team2)) {
+            throw new IllegalArgumentException();
+        }
 
+        int firstTeamIndex = teams.get(team1).index;
+        int secondTeamIndex = teams.get(team2).index;
+        return results[firstTeamIndex][secondTeamIndex];
     }
 
     // is given team eliminated?
     public boolean isEliminated(String team) {
+        if (team == null || !teams.containsKey(team)) {
+            throw new IllegalArgumentException();
+        }
 
+        return true;
     }
 
     // subset R of teams that eliminates given team; null if not eliminated
     public Iterable<String> certificateOfElimination(String team) {
+        if (team == null || teams.containsKey(team)) {
+            throw new IllegalArgumentException();
+        }
 
+        return teams.keySet();
     }
 }
--- Part2/BaseballElimination/src/BaseballEliminationShould.java
+++ Part2/BaseballElimination/src/BaseballEliminationShould.java
@@ -1,26 +1,92 @@
 import org.junit.Assert;
 import org.junit.Test;
-
-import java.util.Arrays;
+import java.util.HashSet;
 
 public class BaseballEliminationShould {
     private final static String team4FileName = "data\\teams4.txt";
+    private final String testTeamName;
+    private final String secondTeamName;
     private BaseballElimination elimination;
 
     public BaseballEliminationShould() {
         elimination = new BaseballElimination(team4FileName);
-    }
-
-    @Test
-    public void ReadAndReturnRightDataFromFileTeam4() {
-        var teams = new String[]{"Atlanta", "Philadelphia", "New_York", "Montreal"};
-        Assert.assertEquals(4, elimination.numberOfTeams());
-        Assert.assertEquals(Arrays.asList(teams), elimination.teams());
-
+        var iterator = elimination.teams().iterator();
+        testTeamName = iterator.next();
+        secondTeamName = iterator.next();
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void ThrowAnExceptionIfAnyArgIsInvalidTeam() {
+        elimination.wins(null);
+    }
 
+    @Test(expected = IllegalArgumentException.class)
+    public void ThrowAnExceptionIfTeamNameIsUnknown() {
+        elimination.wins("Rangers");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void ThrowAnExceptionIfAnyArgIsInvalidTeam1() {
+        elimination.losses(null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void ThrowAnExceptionIfAnyArgIsInvalidTeam2() {
+        elimination.remaining(null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void ThrowAnExceptionIfAnyArgIsInvalidTeam3() {
+        elimination.against(null, testTeamName);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void ThrowAnExceptionIfAnyArgIsInvalidTeam4() {
+        elimination.against(testTeamName, null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void ThrowAnExceptionIfAnyArgIsInvalidTeam5() {
+        elimination.wins(null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void ThrowAnExceptionIfAnyArgIsInvalidTeam6() {
+        elimination.wins(null);
+    }
+
+    @Test
+    public void ReadAndReturnRightDataFromFileTeam4() {
+        Assert.assertEquals(4, elimination.numberOfTeams());
+        var teams = new HashSet<>();
+        teams.add("Atlanta");
+        teams.add("Philadelphia");
+        teams.add("New_York");
+        teams.add("Montreal");
+        var result = new HashSet<>();
+        for (var team:elimination.teams()) {
+            result.add(team);
+        }
+        Assert.assertEquals(teams, result);
+    }
+
+    @Test
+    public void ReturnANumberOfWinsForCurrentFirstTestTeam() {
+        Assert.assertEquals(83, elimination.wins(testTeamName));
+    }
+
+    @Test
+    public void ReturnANumberOfLossesForCurrentFirstTestTeam() {
+        Assert.assertEquals(71, elimination.losses(testTeamName));
+    }
+
+    @Test
+    public void ReturnANumberOfRemainingGamesForCurrentFirstTestTeam() {
+        Assert.assertEquals(8, elimination.remaining(testTeamName));
+    }
+
+    @Test
+    public void ReturnANumberOfRemainingGamesBetweenTestTeamAndSecondTestTeam() {
+        Assert.assertEquals(6, elimination.against(testTeamName, secondTeamName));
     }
 }
