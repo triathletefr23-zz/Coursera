981511047dce1708c8510b1a7b07be3527089702
Pavlo LIAHUSHYN <ur5evw12@gmail.com>
2018-12-21 08:06:29 +0100

    Add implementation for CertificateOfElimination + tests

--- Part2/BaseballElimination/src/BaseballElimination.java
+++ Part2/BaseballElimination/src/BaseballElimination.java
@@ -1,8 +1,6 @@
 import edu.princeton.cs.algs4.*;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Stack;
+import java.util.*;
 
 public class BaseballElimination {
     private final class TeamResult {
@@ -23,6 +21,7 @@ public class BaseballElimination {
     private int sNumber;
     private int tNumber;
     private HashMap<String, TeamResult> teams;
+    private HashMap<Integer, String[]> games;
 
     // create a baseball division from given filename in format specified below
     public BaseballElimination(String filename) {
@@ -37,8 +36,8 @@ public class BaseballElimination {
 
         int iterator = 0;
         while (in.hasNextLine()) {
-            var index = 0;
-            String[] infos = in.readLine().split("\\s+");
+            int index = 0;
+            String[] infos = in.readLine().trim().split("\\s+");
             teams.put(infos[index++],
                     new TeamResult(
                     Integer.parseInt(infos[index++]),
@@ -101,84 +100,6 @@ public class BaseballElimination {
         return results[firstTeamIndex][secondTeamIndex];
     }
 
-    public FlowNetwork CreateFlowNetworkForTheTeam(String team) {
-        TeamResult teamResult = teams.get(team);
-        int currentTeamIndex = teamResult.index;
-        int winAndRemSum = teamResult.wins + teamResult.left;
-
-        int index = teams.size() - 1;
-        int sum = 0;
-        while (index > 0) {
-            sum+=index--;
-        }
-
-        // 2 = s + t
-        int vertices = 2 + sum + teams.size();
-        sNumber = 0;
-        tNumber = vertices - 1;
-
-        // from s to every game + every game has 2 result edges = 3
-        int edges = sum * 3 + teams.size();
-
-        FlowNetwork network = new FlowNetwork(vertices, edges);
-
-        Stack<Integer> usedVertices = new Stack<>();
-        int verticesCount = 1;
-        FlowEdge edge;
-        for (TeamResult team1: teams.values()) {
-            int index1 = team1.index;
-            if (index1 == currentTeamIndex) continue;
-            for (TeamResult team2: teams.values()) {
-                int index2 = team2.index;
-                int gameIndex = index1 * 10 + index2;
-
-                if (index2 == currentTeamIndex ||
-                        index1 == index2 ||
-                        usedVertices.contains(gameIndex)) continue;
-
-                usedVertices.push(gameIndex);
-
-                // from s to remaining games
-                edge = new FlowEdge(sNumber, verticesCount, results[index1][index2]);
-                network.addEdge(edge);
-
-                // from remaining game to first team
-                edge = new FlowEdge(verticesCount, index1 + sum + 1, Double.POSITIVE_INFINITY);
-                network.addEdge(edge);
-
-                // from remaining game to second team
-                edge = new FlowEdge(verticesCount++, index2 + sum + 1, Double.POSITIVE_INFINITY);
-                network.addEdge(edge);
-            }
-
-            // get current team index to get wins count
-            int currentTeamWin = team1.wins;
-            // from every team to t
-            edge = new FlowEdge(index1, tNumber, winAndRemSum - currentTeamWin);
-            network.addEdge(edge);
-        }
-
-        return network;
-    }
-
-    private void RunFordFulkerson() {
-        FordFulkerson algorithm;
-        for (String team: teams.keySet()) {
-            FlowNetwork network = CreateFlowNetworkForTheTeam(team);
-            algorithm = new FordFulkerson(network, sNumber, tNumber);
-        }
-    }
-
-    private String isEliminatedByTrivialReason(String team) {
-        TeamResult teamResult = teams.get(team);
-        int winAndRem = teamResult.wins + teamResult.left;
-        for (String anotherTeam: teams.keySet()) {
-            if (team.equals(anotherTeam)) continue;
-            if (winAndRem < teams.get(anotherTeam).wins) return anotherTeam;
-        }
-        return null;
-    }
-
     // is given team eliminated?
     public boolean isEliminated(String team) {
         if (team == null || !teams.containsKey(team)) {
@@ -188,19 +109,25 @@ public class BaseballElimination {
         String result = isEliminatedByTrivialReason(team);
         if (teams.containsKey(result)) return true;
 
-        return true;
+        ArrayList<Integer> vertices = RunFordFulkerson(team);
+
+        return vertices.size() > 1;
     }
 
     // subset R of teams that eliminates given team; null if not eliminated
     public Iterable<String> certificateOfElimination(String team) {
-        if (team == null || teams.containsKey(team)) {
+        if (team == null || !teams.containsKey(team)) {
             throw new IllegalArgumentException();
         }
 
         String result = isEliminatedByTrivialReason(team);
         if (teams.containsKey(result)) return Collections.singletonList(result);
 
-        return teams.keySet();
+        ArrayList<Integer> vertices = RunFordFulkerson(team);
+
+        if (vertices.size() <= 1) return null;
+
+        return constructCertificateSet(vertices);
     }
 
     public static void main(String[] args) {
@@ -217,5 +144,94 @@ public class BaseballElimination {
                 StdOut.println(team + " is not eliminated");
             }
         }
+    }
+
+    private Iterable<String> constructCertificateSet(ArrayList<Integer> vertices) {
+        HashSet<String> certificate = new HashSet<>();
+        for (Integer elem: vertices) {
+            if (games.containsKey(elem)) {
+                certificate.add(games.get(elem)[0]);
+                certificate.add(games.get(elem)[1]);
+            }
+        }
+        return certificate;
+    }
+
+    private FlowNetwork CreateFlowNetworkForTheTeam(String team) {
+        // remove current team info
+        TeamResult teamResult = teams.remove(team);
+
+        int winAndRemSum = teamResult.wins + teamResult.left;
+
+        int index = teams.size() - 1;
+        int sum = 0;
+        while (index > 0) {
+            sum+=index--;
+        }
+
+        // 2 = s + t
+        int verticesCount = 2 + sum + teams.size();
+        sNumber = 0;
+        tNumber = verticesCount - 1;
+
+        // from s to every game + every game has 2 result edges
+        FlowNetwork network = new FlowNetwork(verticesCount);
+        games = new HashMap<>();
+
+        int verticesIndex = 1;
+        FlowEdge edge;
+        for (Map.Entry<String, TeamResult> key1: teams.entrySet()) {
+            String teamName1 = key1.getKey();
+            TeamResult team1 = key1.getValue();
+            int index1 = team1.index;
+            for (Map.Entry<String, TeamResult> key2: teams.entrySet()) {
+                String teamName2 = key2.getKey();
+                TeamResult team2 = key2.getValue();
+                int index2 = team2.index;
+
+                if (index1 >= index2) continue;
+
+                // from s to remaining games
+                edge = new FlowEdge(sNumber, verticesIndex, results[index1][index2]);
+                network.addEdge(edge);
+                games.put(verticesIndex, new String[] { teamName1, teamName2 });
+
+                // from remaining game to first team
+                edge = new FlowEdge(verticesIndex, index1 + sum + 1, Double.POSITIVE_INFINITY);
+                network.addEdge(edge);
+
+                // from remaining game to second team
+                edge = new FlowEdge(verticesIndex++, index2 + sum + 1, Double.POSITIVE_INFINITY);
+                network.addEdge(edge);
+            }
+
+            edge = new FlowEdge(1 + sum + index1, tNumber, winAndRemSum - team1.wins);
+            network.addEdge(edge);
+        }
+
+        // put again current team info
+        teams.put(team, teamResult);
+
+        return network;
+    }
+
+    private ArrayList<Integer> RunFordFulkerson(String team) {
+        FlowNetwork network = CreateFlowNetworkForTheTeam(team);
+        FordFulkerson algorithm = new FordFulkerson(network, sNumber, tNumber);
+        ArrayList<Integer> minCutVertices = new ArrayList<>();
+        for (int i = 0; i < network.V(); i++) {
+            if (algorithm.inCut(i)) minCutVertices.add(i);
+        }
+        return minCutVertices;
+    }
+
+    private String isEliminatedByTrivialReason(String team) {
+        TeamResult teamResult = teams.get(team);
+        int winAndRem = teamResult.wins + teamResult.left;
+        for (String anotherTeam: teams.keySet()) {
+            if (team.equals(anotherTeam)) continue;
+            if (winAndRem < teams.get(anotherTeam).wins) return anotherTeam;
+        }
+        return null;
     }
 }
--- Part2/BaseballElimination/src/BaseballEliminationShould.java
+++ Part2/BaseballElimination/src/BaseballEliminationShould.java
@@ -1,12 +1,20 @@
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.ArrayList;
 import java.util.HashSet;
 
 public class BaseballEliminationShould {
     private final static String FOUR_TEAMS_FILENAME = "data\\teams4.txt";
+    private final static String FIVE_TEAMS_FILENAME = "data\\teams5.txt";
+    private final static String SEVEN_TEAMS_FILENAME = "data\\teams7.txt";
+    private final static String TWENTY_FOUR_TEAMS_FILENAME = "data\\teams24.txt";
     private final static String DETROIT = "Detroit";
     private final static String MONTREAL = "Montreal";
     private final static String ATLANTA = "Atlanta";
+    private final static String PHILADELPHIA = "Philadelphia";
+    private final static String NEW_YORK = "New_York";
+    private final static String IRELAND = "Ireland";
     private final String testTeamName;
     private final String secondTeamName;
     private BaseballElimination elimination;
@@ -103,12 +111,113 @@ public class BaseballEliminationShould {
 //        Assert.assertNull(elimination.isEliminatedByTrivialReason(ATLANTA));
 //    }
 
+//    @Test
+////    public void ConstructFlowNetworkAtalantaFor4Teams() {
+////        var network = elimination.CreateFlowNetworkForTheTeam(ATLANTA);
+////        var sum = computeSum(elimination.numberOfTeams() - 1);
+////        var result = 3 * sum + elimination.numberOfTeams() - 1;
+////        Assert.assertEquals(8, network.V());
+////        Assert.assertEquals(result, network.E());
+////    }
+////
+////    @Test
+////    public void ConstructFlowNetworkPhiladelphiaFor4Teams() {
+////        var network = elimination.CreateFlowNetworkForTheTeam(PHILADELPHIA);
+////        var sum = computeSum(elimination.numberOfTeams() - 1);
+////        var result = 3 * sum + elimination.numberOfTeams() - 1;
+////        Assert.assertEquals(8, network.V());
+////        Assert.assertEquals(result, network.E());
+////    }
+
     @Test
-    public void ConstructFlowNetworkFor4Teams() {
-        var network = elimination.CreateFlowNetworkForTheTeam(ATLANTA);
-        var sum = computeSum(elimination.numberOfTeams());
-        var expected = 3 * sum + elimination.numberOfTeams();
-        Assert.assertEquals(expected, network.E());
+    public void ReturnTrueForPhiladelphia() {
+        var result = elimination.isEliminated(PHILADELPHIA);
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void ReturnFalseForNew_York() {
+        var result = elimination.isEliminated(NEW_YORK);
+        Assert.assertFalse(result);
+    }
+
+    @Test
+    public void ReturnFalseForAtlanta() {
+        var result = elimination.isEliminated(ATLANTA);
+        Assert.assertFalse(result);
+    }
+
+    @Test
+    public void ReturnFalseForNew_YourFor5Teams() {
+        elimination = new BaseballElimination(FIVE_TEAMS_FILENAME);
+        var result = elimination.isEliminated(NEW_YORK);
+        Assert.assertFalse(result);
+    }
+
+    @Test
+    public void ReturnTrueForDetroitFor5Teams() {
+        elimination = new BaseballElimination(FIVE_TEAMS_FILENAME);
+        var result = elimination.isEliminated(DETROIT);
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void ReturnTrueForAnotherTeamFor5Teams() {
+        elimination = new BaseballElimination(FIVE_TEAMS_FILENAME);
+        var team = elimination.teams().iterator().next();
+        var result = elimination.isEliminated(team);
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void ReturnRWithAtlantaForMontrealFor4Teams() {
+        var certificate = convertIterableToList(elimination.certificateOfElimination(MONTREAL));
+        Assert.assertEquals(1, certificate.size());
+        Assert.assertEquals(ATLANTA, certificate.get(0));
+    }
+
+    @Test
+    public void ReturnRWithAtlantaAndNew_YorkForPhiladelphiaFor4Teams() {
+        var certificate = convertIterableToList(elimination.certificateOfElimination(PHILADELPHIA));
+        Assert.assertEquals(2, certificate.size());
+        Assert.assertTrue(certificate.contains(ATLANTA));
+        Assert.assertTrue(certificate.contains(NEW_YORK));
+    }
+
+    @Test
+    public void ReturnNullForNotEliminatedNew_YorkFor5Teams() {
+        elimination = new BaseballElimination(FIVE_TEAMS_FILENAME);
+        var certificate = elimination.certificateOfElimination(NEW_YORK);
+        Assert.assertNull(certificate);
+    }
+
+    @Test
+    public void ReturnRWith4TeamsForDetroitFor5Teams() {
+        elimination = new BaseballElimination(FIVE_TEAMS_FILENAME);
+        var certificate = convertIterableToList(elimination.certificateOfElimination(DETROIT));
+        Assert.assertEquals(4, certificate.size());
+    }
+
+    @Test
+    public void ReturnTrueForEliminatedIrelandFor7Teams() {
+        elimination = new BaseballElimination(SEVEN_TEAMS_FILENAME);
+        var result = elimination.isEliminated(IRELAND);
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void ReturnTrueForEliminatedTeam13For24Teams() {
+        elimination = new BaseballElimination(TWENTY_FOUR_TEAMS_FILENAME);
+        var result = elimination.isEliminated("Team13");
+        Assert.assertTrue(result);
+    }
+
+    private ArrayList<String> convertIterableToList(Iterable<String> iterable) {
+        ArrayList<String> list = new ArrayList<>();
+        for (var elem: iterable) {
+            list.add(elem);
+        }
+        return list;
     }
 
     private int computeSum(int n) {
